282c26b2ecc69c37a06dbfb12b9f5d68d0774bc0
MouseProducedGames <largemouse@gmail.com>
2020-10-09 17:45:17 -0700

    Add: Local documentation.

--- src/room_hash_map.rs
+++ src/room_hash_map.rs
@@ -11,6 +11,9 @@ use std::default::Default;
 
 // Internal includes.
 
+/// A room which stores its [`TileType`](enum.TileType.html) information in a `HashMap`, indexed by [`LocalPosition`](geometry/struct.LocalPosition.html).
+/// 
+/// The size of the `RoomHashMap` will expand based on the `LocalPosition` provided, as per the specification for [`Room`](trait.Room.html).
 #[derive(Clone)]
 pub struct RoomHashMap {
     local: LocalPosition,
@@ -21,6 +24,9 @@ pub struct RoomHashMap {
 }
 
 impl RoomHashMap {
+    /// Creates a new `RoomHashMap`. As `RoomHashMap` expands to meet its use, no parameters need be supplied.
+    ///
+    /// `RoomHashMap::default()` defers to `RoomHashMap::new()`.
     pub fn new() -> Self {
         Self {
             local: LocalPosition::new(0, 0),
